3c81aeaee1df91b83c18cc855190702a7f67c76f
Shubhi Miradwal <114192364+ShubhiMiradwalNEU@users.noreply.github.com>
2023-12-06 15:00:49 -0500

    Assignment10 23 (#65)
    * deleted webapp.zip
    
    * updated submissionlength
    
    * update githubactions
    
    * updated githubflow
    
    * updated githubworkflow
    
    * updated github workflow
    
    * updated github workflow
    
    * updated readme
    
    * updated Github Action
    
    * updated dev credentials
    
    * updated github action
    
    * updated github work flow
    
    * updated version
    
    * updated route endpoint
    
    * updated version
    
    * updated version to V1
    
    * updated readme
    
    * updated endpoint
    
    * changed the end point

--- app/route/assignment-route.js
+++ app/route/assignment-route.js
@@ -11,35 +11,36 @@ const client = new StatsD({
     port: 8125
 });
 
-router.get('/demo/assignment', (req, res) => {
+router.get('/v3/assignment', (req, res) => {
     assignmentController.getAssignment(req,res);
 });
 
-router.get('/demo/assignment/:id', (req, res) => {
+router.get('/v3/assignment/:id', (req, res) => {
     assignmentController.getAssignmentById(req, res);
 });
 
-router.post('/demo/assignment', (req, res) => {
+router.post('/v3/assignment', (req, res) => {
      assignmentController.postAssignment(req, res);
 });
 
-router.put('/demo/assignment/:id', (req, res) => {
+router.put('/v3/assignment/:id', (req, res) => {
     assignmentController.updateAssignment(req, res);
 });
 
-router.delete('/demo/assignment/:id', (req,res)=>
+router.delete('/v3/assignment/:id', (req,res)=>
 {
     assignmentController.deleteAssignment(req, res);
 });
 
-router.patch('/demo/assignment/:id', (req, res) => {
+
+router.patch('/v3/assignment/:id', (req, res) => {
     logger.info("patch Request for assignement is called");
     client.increment("patch-Request-assignemnt")
     res.status(405).end();
 });
 
-router.post('/demo/assignment/:id/submission', (req, res) => {
 
+router.post('/v3/assignment/:id/submission', (req, res) => {
     assignmentController.submissionDetails(req, res);
 });
 
